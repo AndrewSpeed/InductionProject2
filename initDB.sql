5066e0125ed48c593c5f0347f35536b30ac5d815
Andrew Speed <A.Speed@kainos.com>
2015-06-25 13:10:56 +0100

    Bug in initDB, now USES database.

--- initDB.sql
+++ initDB.sql
@@ -3,6 +3,7 @@ CREATE DATABASE testDB;
 CREATE USER 'testDbUser'@'localhost';
 SET PASSWORD FOR 'testDbUser'@'localhost' = PASSWORD('MySuper5ecretPa55word!');
 GRANT SELECT, UPDATE, INSERT, DELETE ON testDB.* TO 'testDbUser'@'localhost';
+USE testDB;
 
 -- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
 --

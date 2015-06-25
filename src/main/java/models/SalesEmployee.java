d7252491c230a7199c12760b9e1e5f0040bbb175
Andrew Speed <A.Speed@kainos.com>
2015-06-25 10:23:44 +0100

    Bad merge by git

--- src/main/java/models/SalesEmployee.java
+++ src/main/java/models/SalesEmployee.java
@@ -2,17 +2,13 @@ package models;
 
 import java.math.BigDecimal;
 
-<<<<<<< Updated upstream
-public class SalesEmployee {
-=======
 public class SalesEmployee extends BaseEmployee{
->>>>>>> Stashed changes
 
 	private BigDecimal commissionRate;
 	private int salesTotal;
 
 	public SalesEmployee() {
-		
+
 	}
 	public BigDecimal getCommissionRate() {
 		return commissionRate;
